98ba9bc33b85d3928950a6e2e277d23ff064fec0
Zhuchkov <Zhuchkov.vlad@gmail.com>
2017-07-19 01:07:16 +0300

    Add custom tags

---
+++ src/main/java/periodical/view/tag/ContainsFunction.java
@@ -0,0 +1,10 @@
+package periodical.view.tag;
+
+import java.util.List;
+
+public class ContainsFunction {
+	public static boolean contains(List list, Object o){
+		return list.contains(o);
+		
+	}
+}
